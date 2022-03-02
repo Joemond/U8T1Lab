5942864a50277bffc54338bca99d68fc4077120f
BT_1E10_17 <sample@bths.edu>
2022-03-02 10:50:53 -0500

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main (String[] args) {
+        String[] seatingChart = {{Abby, Don, George, Kim, Brian, Elenor, Harry, Lenny, Cathy, Fred, Jill, matt}}
+
+    }
+}
